5755a846cabe6f256f5966134610dddf6ba99d57
Giorgio Borelli <casaborelli@gmail.com>
2012-08-01 11:44:13 +0200

    first import

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+long_description = (
+    open('README.md').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='abstract.markasread',
+      version=version,
+      description="Mark as read functionality for Plone",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['abstract'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
---
+++ src/abstract/markasread/testing.py
@@ -0,0 +1,27 @@
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import applyProfile
+
+from zope.configuration import xmlconfig
+
+class AbstractMarkasread(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE, )
+
+    def setUpZope(self, app, configurationContext):
+        # Load ZCML for this package
+        import abstract.markasread
+        xmlconfig.file('configure.zcml',
+                       abstract.markasread,
+                       context=configurationContext)
+
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'abstract.markasread:default')
+
+ABSTRACT_MARKASREAD_FIXTURE = AbstractMarkasread()
+ABSTRACT_MARKASREAD_INTEGRATION_TESTING = \
+    IntegrationTesting(bases=(ABSTRACT_MARKASREAD_FIXTURE, ),
+                       name="AbstractMarkasread:Integration")
